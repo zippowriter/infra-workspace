2b3464a3c93b8bcbe3f9fce40af13400853a4015
kono_haruhi <kono_haruhi@cyberagent.co.jp>
2023-08-17 11:47:35 +0900

    fix

--- terraform/gcp/iam/main.tf
+++ terraform/gcp/iam/main.tf
@@ -3,7 +3,7 @@ resource "google_service_account" "vertex_account" {
 }
 
 resource "google_project_iam_member" "vertex_account_user" {
-  service_account_id = google_service_account.vertex_account.name
-  role               = "roles/aiplatform.user"
-  member             = "serviceAccount:${google_service_account.vertex_account.email}"
+  project = var.project_id
+  role    = "roles/aiplatform.user"
+  member  = "serviceAccount:${google_service_account.vertex_account.email}"
 }
