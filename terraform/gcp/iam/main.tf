eabd889b36c654fab9f8e56acce7c087c9f86d12
kono_haruhi <kono_haruhi@cyberagent.co.jp>
2023-08-17 11:45:51 +0900

    change the role

--- terraform/gcp/iam/main.tf
+++ terraform/gcp/iam/main.tf
@@ -2,7 +2,7 @@ resource "google_service_account" "vertex_account" {
   account_id = "vertex-account"
 }
 
-resource "google_service_account_iam_member" "vertex_account_user" {
+resource "google_project_iam_member" "vertex_account_user" {
   service_account_id = google_service_account.vertex_account.name
   role               = "roles/aiplatform.user"
   member             = "serviceAccount:${google_service_account.vertex_account.email}"
